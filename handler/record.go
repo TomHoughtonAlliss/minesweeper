03def3f60f7cfebe1063c2a10f7868fceacdd5cd
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2023-11-06 11:53:31 +0000

    Faizan fixed my errors thanks mate

--- handler/record.go
+++ handler/record.go
@@ -1,11 +1,11 @@
 package handler
 
 import (
-	"gopkg.in/mgo.v2/bson"
+	"minesweeper/app/model"
 	"github.com/labstack/echo/v4"
-	"github.com/jamescape/goHackday-minesweeper/model"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func (h *Handler) SaveScore(c echo.Context) (error Error) {
+func (h *Handler) SaveScore(c echo.Context) (err error) {
 	record := &model.Record{ID: bson.NewObjectId()}
 }
