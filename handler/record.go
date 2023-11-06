d0c185b282ba078920d5fc83716d3d2ffedbe569
Tom Houghton-Alliss <tom.houghton-alliss@jamescape.net>
2023-11-06 11:57:54 +0000

    Added handler for records

--- handler/record.go
+++ handler/record.go
@@ -2,10 +2,25 @@ package handler
 
 import (
 	"minesweeper/app/model"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"gopkg.in/mgo.v2/bson"
 )
 
 func (h *Handler) SaveScore(c echo.Context) (err error) {
 	record := &model.Record{ID: bson.NewObjectId()}
+
+	database := h.DB.Clone()
+	defer database.Close()
+
+	// This value will be saved as nil if there is no error
+	insertError := database.DB("minesweeper").C("records").Insert(record)
+	
+	// If the above failed, return the relevant error
+	if insertError != nil {
+		return
+	}
+
+	return c.JSON(http.StatusCreated, record)
 }
